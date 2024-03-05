d72c047465d5105fe3d5596f5e9fbbbcc3b2199a
NurRydberg <nur.rydberg@gmail.com>
2024-03-05 14:47:53 +0100

    auth0 now working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css'
 import { LoginButton } from './components/LoginButton'
+import { LogoutButton } from './components/LogoutButton';
 
 
 function App() {
 
-
+  const {isAuthenticated} = useAuth0();
   return (
     <>
-      <LoginButton />
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
     </>
   )
 }
