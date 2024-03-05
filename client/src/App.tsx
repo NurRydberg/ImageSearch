8466f33d1b800148a2fe6cba0c06273a9a7b4782
NurRydberg <nur.rydberg@gmail.com>
2024-03-05 15:09:11 +0100

    installed axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,18 @@ import { useAuth0 } from '@auth0/auth0-react';
 import './App.css'
 import { LoginButton } from './components/LoginButton'
 import { LogoutButton } from './components/LogoutButton';
+import { useEffect } from 'react';
 
 
 function App() {
+
+  // useEffect(() => {
+  //   const search = async () => {
+  //     const response = await fetch("https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146")
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+  // }, []);
 
   const {isAuthenticated} = useAuth0();
   return (
