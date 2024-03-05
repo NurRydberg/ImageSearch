d583db4b071984b693493cf8c6c69fab0bae8ba4
NurRydberg <nur.rydberg@gmail.com>
2024-03-05 12:01:42 +0100

    Colors installed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const colors = require('colors');
+
 const app = express();
 
-app.listen(3000, () => console.log("Server is up..."))
+app.listen(3000, () => console.log("Server is up...".rainbow.bold.italic))
