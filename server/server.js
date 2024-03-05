4c858d59b3a4ce3ccf702e987bf3bd8d40f322de
NurRydberg <nur.rydberg@gmail.com>
2024-03-05 12:15:40 +0100

    added cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const colors = require('colors');
 
 const app = express();
+app.use(cors());
 
 app.listen(3000, () => console.log("Server is up...".rainbow.bold.italic))
