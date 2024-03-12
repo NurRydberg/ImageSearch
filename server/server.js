36e5a576d8ffc911614c4f34e57f434d75340072
NurRydberg <nur.rydberg@gmail.com>
2024-03-12 14:36:08 +0100

    nu sparar den i filen

--- server/server.js
+++ server/server.js
@@ -17,14 +17,27 @@ app.post ("/users", (req, res) => {
         const dataJsonfile = JSON.parse(fs.readFileSync("./users.json", "utf-8"));
 
         const userExists = dataJsonfile.find((user) => user.email === req.body.email);
-        
-// Finns användaren redan? Då vill vi lägga till hjärtad bild till den användaren. 
-//Om användaren inte finns, spara användaren och bilden i en array
-        dataJsonfile.users.push(req.body);
 
+        if (userExists) {
+            userExists.favoriteImage = req.body.favoriteImage;
+            fs.writeFileSync("./users.json", JSON.stringify(dataJsonfile, null, 2));
+            res.status(200).json("Favorite image added to existing user");
+        } else {
+            dataJsonfile.push(req.body);
+            fs.writeFileSync("./users.json", JSON.stringify(dataJsonfile, null, 2));
+            res.status(201).json("User added with favorite image");
+        }
+
+        // Finns användaren redan? Då vill vi lägga till hjärtad bild till den användaren. 
+        //Om användaren inte finns, spara användaren och bilden i en array
+        dataJsonfile.users.push(req.body);
+        
         fs.writeFileSync("./users.json", JSON.stringify(dataJsonfile, null, 2));
         res.status(201).json("User added");
-    })
+});
+    
+        
+    
     
     app.get("/users/:id", (req, res) => {
         res.send("Varsågod, här kommer användare med id" + req.params.id)
@@ -35,28 +48,7 @@ app.post ("/users", (req, res) => {
 
     app.listen(3000, () => console.log("Server is up...".blue + 
     "Follow this link for page: http://localhost:3000/".rainbow.bold.italic))
+
+
     
     
-    // const user = {
-        //     user: "username",
-        //     favoriteImage: "imageURL",
-        //     password: "password",
-        //     email: "email",
-        // }
-        
-        // const fs = require('fs');
-        // const { finished } = require("stream");
-        
-        // const saveData = (userinformation) => {
-            //     const finished = (error) => {
-                //         if(error){
-                    //             console.error(error);
-//             return;
-//         }
-//     }
-
-//    const jsonData = JSON.stringify(userinformation, null, 2);
-//     fs.writeFileSync('users.json', jsonData, finished)
-// }
-
-// saveData(user);
